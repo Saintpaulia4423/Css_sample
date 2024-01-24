1dadb8b9c042fe4118055f67bf23bd2b22a75c07
Saintpaulia4423 <capitran15428679@gmail.com>
2024-01-24 17:43:46 +0900

    Finish the JavaScript gallery!

---
+++ js/gallery.js
@@ -0,0 +1,29 @@
+//　画像ギャラリー機能を動かす
+//　ここでやりたいのは、ギャラリーの各サムネイル画像にイベントリスナーをアタッチして
+//　クリックされたときにメイン画像をサムネイル画像に対応するものに差し替えること
+function activateGallery() {
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+    // 更新される画像情報
+    let gallerInfo = document.querySelector("#gallery-info");
+    let title = gallerInfo.querySelector(".title");
+    let description = gallerInfo.querySelector(".description");
+
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.addEventListener("click", function () {
+            // クリックされたサムネイル画像をメイン画像として設定する
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            mainImage.setAttribute("src", newImageSrc);
+            mainImage.setAttribute("alt", thumbnail.alt);
+
+            // 現在の画像を変更する
+            const crrentClass = "current";
+            document.querySelector(".current").classList.remove("current");
+            thumbnail.parentNode.classList.add("current");
+
+            // 画像の情報を更新する
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
+}
